01b0ba542ba64a56999a15eb90a610fad0507495
roynaldi19 <roynaldi19@gmail.com>
2021-02-19 20:47:14 +0700

    Initial commit

---
+++ app/src/main/java/com/roynaldi19/bfaa5_08sound/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.roynaldi19.bfaa5_08sound
+
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sp = SoundPool.Builder()
+                .setMaxStreams(10)
+                .build()
+
+        sp.setOnLoadCompleteListener { soundPool, sampleId, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        soundId = sp.load(this, R.raw.clinking_glasses, 1)
+
+        btn_soundpool.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
+        }
+    }
+
+}
